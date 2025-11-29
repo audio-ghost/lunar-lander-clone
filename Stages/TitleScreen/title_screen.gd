3a43fa060b96aa85785e484cf1bbb8fc77926dc4
Colin Kelly <colinkelly3@gmail.com>
2025-11-28 18:41:56 -0600

    Remove high scores button on main menu

--- Stages/TitleScreen/title_screen.gd
+++ Stages/TitleScreen/title_screen.gd
@@ -1,26 +1,20 @@
 extends Control
 
-@onready var start_button: Button = $CenterContainer/VBoxContainer2/VBoxContainer/StartButton
 @onready var level_select_button: Button = $CenterContainer/VBoxContainer2/VBoxContainer/LevelSelectButton
-@onready var high_scores_button: Button = $CenterContainer/VBoxContainer2/VBoxContainer/HighScoresButton
 @onready var settings_button: Button = $CenterContainer/VBoxContainer2/VBoxContainer/SettingsButton
+@onready var test_level_button: Button = $CenterContainer/VBoxContainer2/VBoxContainer/TestLevelButton
 
 func _ready() -> void:
-	start_button.pressed.connect(_on_start_pressed)
 	level_select_button.pressed.connect(_on_level_select_pressed)
-	high_scores_button.pressed.connect(_on_high_scores_pressed)
 	settings_button.pressed.connect(_on_settings_pressed)
-	start_button.grab_focus()
+	test_level_button.pressed.connect(_on_test_level_pressed)
+	level_select_button.grab_focus()
 
-func _on_start_pressed():
+func _on_test_level_pressed():
 	SceneLoader.load_scene("res://Stages/world.tscn")
 
 func _on_level_select_pressed():
 	SceneLoader.load_scene("res://Stages/LevelSelect/level_select.tscn")
-
-func _on_high_scores_pressed():
-	# TODO - Implement High Scores Screen
-	pass
 
 func _on_settings_pressed():
 	# TODO - Implement Settings Screen
